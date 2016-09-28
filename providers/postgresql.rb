2b6c6c70aad4eb958483feb058daa3a4bb933494
Enrique Jimenez Campos <serialsito@gmail.com>
2016-09-28 18:18:57 +0200

    Update postgresql.rb

--- providers/postgresql.rb
+++ providers/postgresql.rb
@@ -14,27 +14,27 @@ action :add do
     netsync = new_resource.netsync
     virtual_ip = new_resource.virtual_ip
 
-    template "#{datadir}/postgresql.conf" do
-      source "postgresql.conf.erb"
-      owner user
-      group group
-      mode 0644
-      cookbook "chef-server"
-      variables(:memory => memory)
-      notifies :restart, "service[postgresql]", :delayed 
-      notifies :reload, "service[opscode-erchef]", :delayed if chef_active
-    end
+#    template "#{datadir}/postgresql.conf" do
+#      source "postgresql.conf.erb"
+#      owner user
+#     group group
+#      mode 0644
+#      cookbook "chef-server"
+#      variables(:memory => memory)
+#      notifies :restart, "service[postgresql]", :delayed 
+#      notifies :reload, "service[opscode-erchef]", :delayed if chef_active
+#    end
 
-    template "#{datadir}/pg_hba.conf" do
-      source "pg_hba.conf.erb"
-      owner user
-      group group
-      mode 0644
-      cookbook "chef-server"
-      variables(:netsync => netsync, :virtual => virtual_ip)
-      notifies :restart, "service[postgresql]", :delayed 
-      notifies :reload, "service[opscode-erchef]", :delayed if chef_active
-    end
+#    template "#{datadir}/pg_hba.conf" do
+#      source "pg_hba.conf.erb"
+#      owner user
+#      group group
+#      mode 0644
+#      cookbook "chef-server"
+#      variables(:netsync => netsync, :virtual => virtual_ip)
+#      notifies :restart, "service[postgresql]", :delayed 
+#      notifies :reload, "service[opscode-erchef]", :delayed if chef_active
+#    end
 
 
    # if srmode == "master"
