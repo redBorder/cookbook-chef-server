0b981785b0ff5f318b198875b3585d14c0958fdb
Miguel <manegron@redborder.com>
2022-02-18 11:27:09 +0000

    Add memory variable for rabbitmq

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -8,6 +8,7 @@ action :add do
     memory = new_resource.memory
     chef_active = new_resource.chef_active
     rabbitmq = new_resource.rabbitmq
+    rabbitmq_memory = new_resource.rabbitmq_memory
     postgresql = new_resource.postgresql
     postgresql_memory = new_resource.postgresql_memory
     postgresql_vip = new_resource.postgresql_vip
