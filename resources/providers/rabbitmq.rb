c4dfbae0488b40c954404793883fa6802ab1f66a
Miguel <manegron@redborder.com>
2022-02-18 14:34:58 +0000

    Dont create config file

--- resources/providers/rabbitmq.rb
+++ resources/providers/rabbitmq.rb
@@ -11,25 +11,25 @@ action :add do
     user = new_resource.user
     group = new_resource.group
 
-    template "#{config_dir}/rabbitmq.config" do
-      source "rabbitmq.config.erb"
-      owner user
-      group group
-      mode 0644
-      cookbook "chef-server"
-      variables(:memory => memory)
-    end
-
-    # template "#{config_dir}/rabbitmq.conf" do
-    #   source "rabbitmq.conf.erb"
+    # template "#{config_dir}/rabbitmq.config" do
+    #   source "rabbitmq.config.erb"
     #   owner user
     #   group group
     #   mode 0644
     #   cookbook "chef-server"
     #   variables(:memory => memory)
-    #   notifies :restart, "service[opscode-rabbitmq]", :delayed
-    #   notifies :reload, "service[opscode-erchef]", :delayed if chef_active
-    # end 
+    # end
+
+    template "#{config_dir}/rabbitmq.conf" do
+      source "rabbitmq.conf.erb"
+      owner user
+      group group
+      mode 0644
+      cookbook "chef-server"
+      variables(:memory => memory)
+      notifies :restart, "service[opscode-rabbitmq]", :delayed
+      notifies :reload, "service[opscode-erchef]", :delayed if chef_active
+    end 
 
     service "opscode-rabbitmq" do
       service_name "opscode-rabbitmq"
