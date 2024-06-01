4fc36e069d216329ae007d90c9861bcb4798a5d0
Miguel Negron <manegron@redborder.com>
2024-06-01 16:03:08 +0100

    lint providers

--- resources/providers/postgresql.rb
+++ resources/providers/postgresql.rb
@@ -1,7 +1,5 @@
-# Cookbook Name:: Postgresql
-#
+# Cookbook:: Postgresql
 # Provider:: chef
-#
 
 action :add do
   begin
@@ -9,47 +7,44 @@ action :add do
     datadir = new_resource.datadir
     user = new_resource.user
     group = new_resource.group
-    srmode = new_resource.srmode
+    # srmode = new_resource.srmode
     chef_active = new_resource.chef_active
     netsync = new_resource.netsync
     virtual_ip = new_resource.virtual_ip
 
-   template "#{datadir}/postgresql.conf" do
-     source "postgresql.conf.erb"
-     owner user
-     group group
-     mode 0644
-     cookbook "chef-server"
-     variables(:memory => memory)
-     notifies :restart, "service[postgresql]", :delayed
-     notifies :reload, "service[opscode-erchef]", :delayed if chef_active
-   end
+    template "#{datadir}/postgresql.conf" do
+      source 'postgresql.conf.erb'
+      owner user
+      group group
+      mode '0644'
+      cookbook 'chef-server'
+      variables(memory: memory)
+      notifies :restart, 'service[postgresql]', :delayed
+      notifies :reload, 'service[opscode-erchef]', :delayed if chef_active
+    end
 
-   template "#{datadir}/pg_hba.conf" do
-     source "pg_hba.conf.erb"
-     owner user
-     group group
-     mode 0644
-     cookbook "chef-server"
-     variables(:netsync => netsync, :virtual => virtual_ip)
-     notifies :restart, "service[postgresql]", :delayed
-     notifies :reload, "service[opscode-erchef]", :delayed if chef_active
-   end
+    template "#{datadir}/pg_hba.conf" do
+      source 'pg_hba.conf.erb'
+      owner user
+      group group
+      mode '0644'
+      cookbook 'chef-server'
+      variables(netsync: netsync, virtual: virtual_ip)
+      notifies :restart, 'service[postgresql]', :delayed
+      notifies :reload, 'service[opscode-erchef]', :delayed if chef_active
+    end
 
+    # if srmode == 'master'
+    # else
+    # end
 
-   if srmode == "master"
-   
-   else
-
-   end
-
-    service "opscode-postgresql" do
-      service_name "opscode-postgresql"
-      supports :status => true, :reload => true, :restart => true, :start => true, :enable => true
+    service 'opscode-postgresql' do
+      service_name 'opscode-postgresql'
+      supports status: true, reload: true, restart: true, start: true, enable: true
       action [:enable, :start]
     end
 
-    Chef::Log.info("Chef services has been configurated correctly.")
+    Chef::Log.info('Chef services has been configurated correctly.')
   rescue => e
     Chef::Log.error(e)
   end
@@ -57,9 +52,8 @@ end
 
 action :remove do
   begin
-    #logdir = new_resource.logdir
-
-    Chef::Log.info("Chef services has been deleted correctly.")
+    # logdir = new_resource.logdir
+    Chef::Log.info('Chef services has been deleted correctly.')
   rescue => e
     Chef::Log.error(e)
   end
