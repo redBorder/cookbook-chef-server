537dffe08d5444a2f2e5dee98ce62f796b5ec58b
Miguel Negron <manegron@redborder.com>
2024-06-01 16:03:02 +0100

    lint attributes

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -1,33 +1,11 @@
-default["chef-server"]["services"]["chef"] = true
-default["chef-server"]["services"]["nginx"] = true
-default["chef-server"]["services"]["postgresql"] = true
+default['chef-server']['services']['chef'] = true
+default['chef-server']['services']['nginx'] = true
+default['chef-server']['services']['postgresql'] = true
+default['chef-server']['chef_middleware'] = %w(bookshelf oc_bifrost oc_id opscode-erchef redis_lb opensearch)
+default['chef-server']['services_list'] = %w(postgresql nginx bookshelf oc_bifrost oc_id opscode-erchef redis_lb opensearch)
+default['chef-server']['services_dir'] = '/opt/opscode/service'
 
-default["chef-server"]["chef_middleware"] = [
-                                        "bookshelf",
-                                        "oc_bifrost",
-                                        "oc_id",
-                                        "opscode-erchef",
-                                        "redis_lb",
-                                        "opensearch"
-                                      ]
-
-
-
-
-default["chef-server"]["services_list"] = [
-                                            "postgresql",
-                                            "nginx",
-                                            "bookshelf",
-                                            "oc_bifrost",
-                                            "oc_id",
-                                            "opscode-erchef",
-                                            "redis_lb",
-                                            "opensearch"
-                                          ]
-
-default["chef-server"]["services_dir"] = "/opt/opscode/service"
-
-#flags
-default["chef-server"]["installed"] = false
-default["chef-server"]["datastore_configured"] = false
-default["chef-server"]["registered"] = false
+# flags
+default['chef-server']['installed'] = false
+default['chef-server']['datastore_configured'] = false
+default['chef-server']['registered'] = false
